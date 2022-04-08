28fc7cb4c4df638f00c1e520e51c866b7dea7210
Idegbekwu Oluwaseyi <IdegbekwuOluwaseyi@example.com>
2022-04-08 06:13:38 -0700

    hello, put Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+# include <stdio.h>
+# include <string.h>
 
 /**
- * Main -Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-  char str1[55];
+char str1[55];
 
-  strcpy(str1, "\"Programming is like building a multilingual puzzle");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
-  puts(str1);
 
-  return (0);
+puts(str1);
+
+return (0);
 }
